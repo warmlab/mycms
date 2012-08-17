efb0a18c3afa7b156a5ffd048eae6aff9d0f1c8a
warmlab <xli@websense.com>
2012-08-17 13:48:06 +0800

    Add disqus comment

--- mycms/settings.py
+++ mycms/settings.py
@@ -128,7 +128,11 @@ INSTALLED_APPS = (
 	'gunicorn',
 	'codes',
 	'about',
+	'disqus',
 )
+
+DISQUS_API_KEY = None
+DISQUS_WEBSITE_SHORTNAME = 'warmlab'
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
